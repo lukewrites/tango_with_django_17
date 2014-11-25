7e48533f664efdb533c39b19b412901d6cda000f
Luke Petschauer <luke@lukewrites.com>
2014-11-25 16:23:39 +0800

    fixed a silly mistake

--- rango/forms.py
+++ rango/forms.py
@@ -63,5 +63,5 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('website', 'pictures')
+        fields = ('website', 'picture')
 
