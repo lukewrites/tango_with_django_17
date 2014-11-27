2b2c07625078cd19ea33cb85a285a8cd86a2ab80
Luke Petschauer <luke@lukewrites.com>
2014-11-27 10:32:38 +0800

    CH 12: django-registration-redux

--- rango/forms.py
+++ rango/forms.py
@@ -37,20 +37,20 @@ class PageForm(forms.ModelForm):
         fields = ('title', 'url')
         # Do NOT include
 
-    # def clean(self):
-    #     cleaned_data = self.cleaned_data
-    #     # cleaned_data is a ModelForm dictionary attribute.
-    #     url = cleaned_data.get('url')
-    #     # we use the dictionary.get() method to get the form's value for url
-    #
-    #     # if url isn't empty and doesn't start with http://
-    #     if url and not url.startswith('http://'):
-    #         url = 'http://' + url
-    #         # we reassign the value in the dictionary with our new, clean url
-    #         cleaned_data['url'] = url
-    #     # we always have to end the clean() method by returning the cleaned_data dictionary.
-    #     # always, always, always, always, always.
-    #     return cleaned_data
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        # cleaned_data is a ModelForm dictionary attribute.
+        url = cleaned_data.get('url')
+        # we use the dictionary.get() method to get the form's value for url
+
+        # if url isn't empty and doesn't start with http://
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            # we reassign the value in the dictionary with our new, clean url
+            cleaned_data['url'] = url
+        # we always have to end the clean() method by returning the cleaned_data dictionary.
+        # always, always, always, always, always.
+        return cleaned_data
 
 
 class UserForm(forms.ModelForm):
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
-)
+    (r'^accounts/', include('registration.backends.simple.urls')),
+    )
 if settings.DEBUG:
     urlpatterns += patterns(
         'django.views.static', (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT},)
